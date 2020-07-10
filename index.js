e43232aef90678d9884c08b8244d9106d56bbf04
rodrigo.rio@outlook.com.ar <1478542a>
2020-07-10 01:48:45 -0300

    + Removido shadding

--- index.js
+++ index.js
@@ -1,8 +1,117 @@
 require('dotenv').config({path: __dirname + '/.env'});
+const Discord = require('discord.js');
+const fs = require('fs');
+const client = new Discord.Client();
 
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: process.env['TOKEN'] });
+const app = require('./src');
 
-manager.spawn();
-// manager.on('shardCreate', shard => console.log(`Launched shard id(s) ${shard.ids.join(', ')}`));
-// manager.on('shardCreate', shard => console.log(shard.id));
+client.login(process.env['TOKEN']).then( () => {
+    client.on('message', mensaje => {
+
+        if (mensaje.content.includes('c!')) {
+
+            let args = [];
+            let comando = mensaje.content.split('c!')[1];
+
+            // SI EL STRING TIENE ESPACIOS
+            if (/\s/.test(comando)) {
+
+                args = comando.split(' '); // DIVIDIMOS EN ARRAYS POR ESPACIOS
+                comando = args.shift(); // SACAMOS EL PRIMER ITEM QUE SERÍA EL COMANDO, LO DEMÁS SON LOS ARGUMENTOS QUE LE SIGUEN
+            }
+
+            if (comando.length > 1) {
+
+                leerComando(comando, args, mensaje).then( (res) => {
+                    mensaje.reply(res);
+                }).catch( (err) => {
+                    mensaje.reply(err);
+                });
+
+            }
+        }
+    });
+
+    client.on("ready", async () => {
+        console.log("Coscu BOT by RodrigoRio");
+        console.log("Node Version: " + process.version);
+        console.log("Discord.js Version: " + Discord.version);
+
+        await client.user.setActivity((client.guilds.cache.size).toString() + " servers c!help", {type: "PLAYING"});
+    });
+
+    client.on("guildCreate", async () => {
+        await client.user.setActivity((client.guilds.cache.size).toString() + " servers c!help", {type: "PLAYING"});
+    });
+}).catch(console.error);
+
+async function leerComando(comando, args, mensaje) {
+
+    return new Promise( async (success, failure) => {
+
+        if(mensaje.member.id !== client.user.id) {
+            switch (comando) {
+                case 'help':
+                    let msg = "c!<frase>: dice alguna frase de coscu (Ej: c!buenardo) (SÓLO FUNCIONA EN MODO MANUAL)\n" +
+                        "c!manual: El bot solo va a funcionar por comando\n" +
+                        "c!automatico <tiempo_en_segundos>: El bot va a ingresar a todos los channels cada X tiempo a reproducir un sonido al azar\n" +
+                        "c!escuchar: El bot va a escucharte cada 10 segundos, 3 segundos. Si decís una frase de Coscu (Ej: buenardo, clave) el bot va a reproducir la frase sólo (Deshabilitado momentaneamente por cuestiones de escalabilidad) \n" +
+                        "c!sonidos: Muestra los sonidos disponibles para reproducir";
+
+                    success(msg);
+
+                    break;
+                case 'automatico':
+                    app.automatico.modoAutomatico(true, args, mensaje).then( (suc) => {
+                        success(suc);
+                    }).catch( (err) => {
+                        failure(err);
+                    });
+                    break;
+                case 'manual':
+                    app.automatico.modoAutomatico(false, args, mensaje).then( (suc) => {
+                        success(suc);
+                    }).catch( (err) => {
+                        failure(err);
+                    });
+                    break;
+
+                case 'sonidos':
+
+                    let audios = [];
+
+                    await fs.readdir('./audios/', (err, archivos) => {
+
+                        archivos.forEach(archivo => {
+                            audios.push(archivo.replace('.mp3', ''));
+                        });
+
+                        success("Todos los sonidos que hay para reproducir son: " + audios.join(' - '));
+                    });
+
+                    break;
+
+                // case 'escuchar': app.escuchar.agregarEscucha(mensaje); break;
+
+                default:
+
+                    const voiceChannel = mensaje.member.voice.channel;
+
+                    if (!voiceChannel) failure('Bbto metete a un chanel para escucharme');
+                    if (!fs.existsSync('./audios/' + comando + '.mp3')) failure('mMm ese comandovich no lo tengo');
+
+                    // SI ESTÁ EN MODO AUTOMÁTICO
+                    const automatico = app.data.automatico.get(mensaje.guild.id);
+                    if (automatico) failure('El bot está en modo automático brEEEo, desactivalo con c!manual');
+
+                    app.sonidos.agregarCola(comando, mensaje).then( (suc) => {
+                        success(suc);
+                    }).catch( (err) => {
+                        failure(err);
+                    });
+
+                    break;
+            }
+        }
+    });
+}
